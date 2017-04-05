7d6dc14c6f0d99962f8dee7262f21dc6abf55162
optimizely-christine <christine.perry@optimizely.com>
2017-04-04 20:26:56 -0700

    Ruby Full Stack Product

---
+++ bin/app.rb
@@ -0,0 +1,74 @@
+require 'sinatra'
+require 'httparty'
+require 'optimizely'
+require 'csv_hasher'
+
+url = 'https://cdn.optimizely.com/json/7681190327.json'
+datafile = HTTParty.get(url).body
+optimizely = Optimizely::Project.new(datafile)
+
+
+set :port, 8080
+set :static, true
+set :public_folder, "public"
+set :views, "views"
+
+class Item <
+  Struct.new(:name, :color, :category, :price, :imageUrl)
+end
+
+def build_items()
+  items = Array.new
+  f = File.open("bin/items.csv", "r")
+  f.each_line { |line|
+
+    # each line has fields separated by commas, so split those fields
+    fields = line.split(',')
+
+    # create a new Person
+    item = Item.new
+
+    # do a little work here to get rid of double-quotes and blanks
+    item.name = fields[0]
+    item.color = fields[1]
+    item.category = fields[2]
+    item.price = fields[3]
+    item.imageUrl = fields[4]
+    items.push(item)
+  }
+  return items
+end
+
+get '/' do
+  items = build_items()
+
+  erb :index, :locals => {'user_id' => '', 'filter_by' => '', 'feature_display' => 'none', 'data' => items}
+
+end
+
+post '/shop' do
+  user_id = params[:user_id]
+  filter_by = params[:filter]
+  feature_display = 'none'
+  items = build_items()
+
+  variation_key = optimizely.activate('feature_rollout', user_id)
+  if variation_key == 'holdback'
+    feature_display = 'none'
+  elsif variation_key == 'new_feature'
+    # execute code for new_feature
+    feature_display = 'block'
+    items = items.sort_by { |k| k[filter_by].to_i }
+  else
+    # execute default code
+    feature_display = 'none'
+  end
+
+  erb :index, :locals => {'user_id' => user_id, 'filter_by' => filter_by, 'feature_display' => feature_display, 'data' => items}
+end
+
+post '/buy' do
+  user_id = params[:user_id]
+  optimizely.track('purchased_item', user_id)
+  erb :buy, :locals => {}
+end
